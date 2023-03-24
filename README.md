a1294043e7ba85dbc535b1db37e835efe0637a1d
LeShush <mycelium.architecture@gmail.com>
2023-03-23 19:58:09 -0600

    Third Commit - Code updated

--- README.md
+++ README.md
@@ -23,24 +23,24 @@ SO THAT I can create a strong password that provides greater security
 
 GIVEN I need a new, secure password:
 
-
-WHEN I click the button to generate a password.
+🟢
+🟡WHEN I click the button to generate a password.
 THEN I am presented with a series of prompts for password criteria.
 
-WHEN prompted for password criteria
+🟡WHEN prompted for password criteria
 THEN I select which criteria to include in the password.
 
-WHEN prompted for the length of the password
+🟡WHEN prompted for the length of the password
 THEN I choose a length of at least 8 characters and no more than 128 characters.
 
-WHEN asked for character types to include in the password
+🟡WHEN asked for character types to include in the password
 THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters.
 
-WHEN I answer each prompt
+🟡WHEN I answer each prompt
 THEN my input should be validated and at least one character type should be selected.
 
-WHEN all prompts are answered
+🟡WHEN all prompts are answered
 THEN a password is generated that matches the selected criteria.
 
-WHEN the password is generated
+🟡WHEN the password is generated
 THEN the password is either displayed in an alert or written to the page
--- assets/JS/script.js
+++ assets/JS/script.js
@@ -1,12 +1,70 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var char = "abcdefghijklmnopqrstuvwxyz";
+var charMay = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var charNum = "0123456789";
+var charSpec = ` !"#$%&'()*+,-./:;<=>?@[]^_{|}~`;
+var password = "";
+
+/*
+function pieceCharacters (x) {
+
+    for (var i = 0; i <= psswdLength; i++) {
+        var randomNumber = Math.floor(Math.random() * x.length);
+        password += x.substring(randomNumber, randomNumber + 1);
+       } 
+       return password;
+} */
+
+//Generates password
+function generatePassword() {
+    // Define the length of the password
+  var psswdLength = prompt("Define the length of your password", 8);
+  console.log(psswdLength);
+
+  // Begins the prompt to the user
+  var confUpper = confirm("Default passwords include lowercase letters only. Would you like to add Uppercase letters?");
+  console.log(confUpper);
+  if (confUpper === true) {
+    var char = "abcdefghijklmnopqrstuvwxyz";
+    char = char.concat(charMay);
+    console.log(char);
+    var confNum = confirm("Numbers?");
+
+    if (confNum === true) {
+        var char = "abcdefghijklmnopqrstuvwxyz";
+        char = char.concat(charNum);
+        console.log(charLowUpNum);
+        confirm("Special Characters?");
+
+    } else {
+        var char = "abcdefghijklmnopqrstuvwxyz";
+        for (var i = 0; i <= psswdLength; i++) {
+            var randomNumber = Math.floor(Math.random() * char.length);
+            password += char.substring(randomNumber, randomNumber + 1);
+           } 
+           return password;
+    }
+
+  } else {
+    var char = "abcdefghijklmnopqrstuvwxyz";
+    console.log()
+    for (var i = 0; i <= psswdLength; i++) {
+        var randomNumber = Math.floor(Math.random() * char.length);
+        password += char.substring(randomNumber, randomNumber + 1);
+       }
+    return password;
+  }
+
+} 
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
+  var psswd = generatePassword();
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
+  passwordText.value = psswd;
+  
 
 }
 
